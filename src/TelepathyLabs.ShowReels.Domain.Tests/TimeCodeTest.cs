a2f75b9b669e3f51b533a39d3e90b0107502fd71
nisal-wickramage <nisalwickramage@gmail.com>
2022-07-01 17:35:47 +0800

    Implement timecode and tests

---
+++ src/TelepathyLabs.ShowReels.Domain.Tests/TimeCodeTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+
+namespace TelepathyLabs.ShowReels.Domain.Tests
+{
+    public class TimeCodeTest
+    {
+        private const int framesPerSecondForNTSC = 30;
+        private const int framesPerSecondForPAL = 25;
+
+        public TimeCodeTest()
+        {
+        }
+
+        [Fact]
+        public void NumberOfFramesShouldBeLessThanFramesPerSecond()
+        {
+            Assert.Throws<Exception>(() => new TimeCode(1, 1, 1, 30, framesPerSecondForNTSC));
+            Assert.True(new TimeCode(1, 1, 1, framesPerSecondForNTSC - 1, framesPerSecondForNTSC) != null);
+        }
+
+        [Fact]
+        public void TimeCodesWithSameIndividualComponentsShouldBeEqual()
+        {
+            var timeCode1 = new TimeCode(1, 1, 1, 1, framesPerSecondForNTSC);
+            var timeCode2 = new TimeCode(1, 1, 1, 1, framesPerSecondForNTSC);
+            var timeCode3 = new TimeCode(2, 1, 1, 1, framesPerSecondForNTSC);
+
+            Assert.True(timeCode1.Equals(timeCode2));
+            Assert.False(timeCode1.Equals(timeCode3));
+        }
+
+        [Fact]
+        public void AddingTwoTimeCodesShouldResultInAdditionOfIndividualComponents()
+        {
+            var timeCode1 = new TimeCode(1, 1, 1, 1, framesPerSecondForNTSC);
+            var timeCode2 = new TimeCode(1, 1, 1, 1, framesPerSecondForNTSC);
+
+            Assert.True(new TimeCode(2, 2, 2, 2, framesPerSecondForNTSC).Equals(timeCode1.Add(timeCode2)));
+        }
+
+        [Fact]
+        public void AddingTimeCodesWithDifferentFrameRatesShouldFail()
+        {
+            var timeCode1 = new TimeCode(1, 1, 1, 1, framesPerSecondForNTSC);
+            var timeCode2 = new TimeCode(1, 1, 1, 1, framesPerSecondForPAL);
+
+            Assert.Throws<Exception>(() => timeCode1.Add(timeCode2));
+        }
+
+        [Fact]
+        public void ComparisonOfTwoTimeCodesShouldGiveCorrectHighest()
+        {
+            var timeCode1 = new TimeCode(1, 1, 1, 1, framesPerSecondForNTSC);
+            var timeCode2 = new TimeCode(2, 1, 1, 1, framesPerSecondForNTSC);
+
+            Assert.True(timeCode2 > timeCode1);
+            Assert.False(timeCode2 < timeCode1);
+        }
+
+        [Fact]
+        public void TimecodesWithDifferentFrameRatesShouldFailComparison()
+        {
+            var timeCode1 = new TimeCode(1, 1, 1, 1, framesPerSecondForPAL);
+            var timeCode2 = new TimeCode(2, 1, 1, 1, framesPerSecondForNTSC);
+
+            Assert.Throws<Exception>(() => timeCode1 < timeCode2);
+            Assert.Throws<Exception>(() => timeCode2 > timeCode1);
+
+        }
+    }
+}
+
---
+++ src/TelepathyLabs.ShowReels.Domain/TimeCode.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace TelepathyLabs.ShowReels.Domain
+{
+    public class TimeCode
+    {
+        public TimeCode(int hours, int minutes, int seconds, int frames, int framesPerSecond)
+        {
+            if (frames >= framesPerSecond)
+            {
+                throw new Exception("Frames should be less than frame rate.");
+            }
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+            Frames = frames;
+            FramesPerSecond = framesPerSecond;
+        }
+
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+        public int Frames { get; private set; }
+        public int FramesPerSecond { get; private set; }
+        public int TotalFrames
+        {
+            get
+            {
+                return Frames + (Seconds + Minutes * 60 + Hours * 3600) * FramesPerSecond;
+            }
+        }
+
+        public TimeCode Add(TimeCode timeCode)
+        {
+            if (timeCode.FramesPerSecond != FramesPerSecond)
+            {
+                throw new Exception("Frame rates doesn't match.");
+            }
+            var totalFrames = Frames + timeCode.Frames;
+            var framesToPromote = totalFrames / FramesPerSecond;
+            var frames = totalFrames % FramesPerSecond;
+
+            var totalSeconds = Seconds + timeCode.Seconds + framesToPromote;
+            var secondsToPromote = totalSeconds / 60;
+            var seconds = totalSeconds % 60;
+
+            var totalMinutes = Minutes + timeCode.Minutes + secondsToPromote;
+            var minutesToPromote = totalMinutes / 60;
+            var minutes = totalMinutes % 60;
+
+            var totalHours = Hours + timeCode.Hours + minutesToPromote;
+
+            return new TimeCode(totalHours, minutes, seconds, frames, FramesPerSecond);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj is not TimeCode)
+            {
+                return false;
+            }
+
+            return obj is TimeCode timeCode &&
+                FramesPerSecond == timeCode.FramesPerSecond &&
+                Hours == timeCode.Hours && Minutes == timeCode.Minutes && Seconds == timeCode.Seconds && Frames == timeCode.Frames;
+
+        }
+
+        public static bool operator <(TimeCode a, TimeCode b)
+        {
+            if (a.FramesPerSecond != b.FramesPerSecond)
+            {
+                throw new Exception("Frame rates doesn't match.");
+            }
+
+            return a.TotalFrames < b.TotalFrames;
+        }
+
+
+        public static bool operator >(TimeCode a, TimeCode b)
+        {
+            if (a.FramesPerSecond != b.FramesPerSecond)
+            {
+                throw new Exception("Frame rates doesn't match.");
+            }
+
+            return a.TotalFrames > b.TotalFrames;
+        }
+    }
+}
+
