b4108364c564fdc55077c23f72a85e9ee317ef22
nisal-wickramage <nisalwickramage@gmail.com>
2022-07-08 07:25:07 +0800

    Create show reel object from form value

--- src/TelepathyLabs.ShowReels.Web/src/app/components/show-reel-editor/show-reel-editor.component.ts
+++ src/TelepathyLabs.ShowReels.Web/src/app/components/show-reel-editor/show-reel-editor.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { VideoStandardService } from '../../services/video-standards/video-standard.service';
 import { VideoDefinitionService } from '../../services/video-definitions/video-definition.service';
 import { ShowReelsService } from '../../services/ShowReels/show-reels.service';
-import { FormGroup, FormControl , FormBuilder, FormArray} from '@angular/forms';
+import { FormBuilder, FormArray} from '@angular/forms';
 import { KeyValuePair } from 'src/app/services/models/key-value-pair';
+import { VideoClip } from 'src/app/services/models/video-clip';
+import { TimeCode } from 'src/app/services/models/time-code';
+import { ShowReel } from 'src/app/services/models/show-reel';
 
 @Component({
   selector: 'app-show-reel-editor',
@@ -11,8 +14,11 @@ import { KeyValuePair } from 'src/app/services/models/key-value-pair';
   styleUrls: ['./show-reel-editor.component.css']
 })
 export class ShowReelEditorComponent implements OnInit {
+  frameRates = new Map<number, number>();
+
   showReelForm = this.formBuilder.group({
     name: [''],
+    description: [''],
     videoDefinition: [''],
     videoStandard: [''],
     totalDuration: [''],
@@ -41,7 +47,10 @@ export class ShowReelEditorComponent implements OnInit {
     private videoDefinitionService: VideoDefinitionService,
     private videoStandardService: VideoStandardService,
     private showReelService: ShowReelsService,
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder) {
+      this.frameRates.set(1, 25);
+      this.frameRates.set(2, 30);
+     }
 
   ngOnInit(): void {
     this.videoDefinitionService.get().subscribe(response => {
@@ -81,7 +90,33 @@ export class ShowReelEditorComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.showReelForm.value);
+    let clips = this.showReelForm.value.videoClips?.map(v => {
+      var startTimeCode = new TimeCode(
+        parseInt(v.clipStartTimeCodeHours??''),
+        parseInt(v.clipStartTimeCodeMinutes??''),
+        parseInt(v.clipStartTimeCodeSeconds??''),
+        parseInt(v.clipStartTimeCodeFrames??''),
+        this.frameRates.get(parseInt(v.clipVideoStandard??''))??0
+      );
+      var endTimeCode = new TimeCode(
+        parseInt(v.clipEndTimeCodeHours??''),
+        parseInt(v.clipEndTimeCodeMinutes??''),
+        parseInt(v.clipEndTimeCodeSeconds??''),
+        parseInt(v.clipEndTimeCodeFrames??''),
+        this.frameRates.get(parseInt(v.clipVideoStandard??''))??0
+      );
+
+      return new VideoClip(v.clipName ?? '', v.clipDescription ?? '', parseInt(v.clipVideoDefinition??''), parseInt(v.clipVideoStandard??''), startTimeCode, endTimeCode);
+    });
+
+    var showReel = new ShowReel(
+      this.showReelForm.value.name ?? '',
+      this.showReelForm.value.description ?? '',
+      parseInt(this.showReelForm.value.videoDefinition ?? ''),
+      parseInt(this.showReelForm.value.videoStandard ?? ''),
+      clips ?? []
+    );
+    console.log(showReel);
   }
 
 }
